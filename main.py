3ee7b1269f35f87fac20a1fbc05a11d0ad48678f
Craig Wm. Versek <cversek@gmail.com>
2017-01-02 19:29:58 -0500

    added exception handling and error loging to the loop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #upython standard libraries
-import time, ujson
+import sys, time, ujson, gc, micropython
 
 #local imports
 from am2315 import AM2315
@@ -8,11 +8,13 @@ from data_stream import DataStreamClient
 #DEBUG = False
 DEBUG = True
 
-SAMPLE_DELAY = 10 #seconds
+SAMPLE_DELAY = 1 #seconds
 
 #read the SECRET configuration file, NOTE this contains PRIVATE keys and 
 #should never be posted online
 config = ujson.load(open("SECRET_CONFIG.json",'r'))
+
+
 
 #configure sensor interface
 ht_sensor = AM2315()
@@ -24,28 +26,32 @@ dsc = DataStreamClient(host=dbs['host'],
                        public_key=dbs['public_key'],
                        private_key=dbs['private_key'])
 
+#dsc.open_connection()
 d = {} #stores sample data
 while True:
-    #acquire a data sample
-    ht_sensor.get_data(d)
+    try:
+        #acquire a data sample
+        ht_sensor.get_data(d)
+        if DEBUG:
+            print(d)
+        #push data to the data stream
+        reply = dsc.push_data(d.items())
+        if DEBUG:
+            print(reply)
+    except Exception as exc:
+        #write error to log file
+        errorlog = open("errorlog.txt",'w')
+        sys.print_exception(exc, errorlog)
+        errorlog.close()
+        if DEBUG:
+            sys.print_exception(exc) #print to stdout
+            #re raise the exception to halt the program
+            raise exc
+    #force garbage collection
+    gc.collect()
+    #print some debugging info
     if DEBUG:
-        print(d)
-    #push data to the data stream
-    reply = dsc.push_data(d.items())
-    if DEBUG:
-        print(reply)
+        print("Memory Free: %d" % gc.mem_free())
+        print(micropython.mem_info())
     #wait a bit before taking another sample
     time.sleep(SAMPLE_DELAY)
-
-
-
-
-
-
-
-
-
-
-
-
-
