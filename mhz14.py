3f5f5d81701b21058802b8c13a06ecd0516f3b42
Craig Wm. Versek <cversek@gmail.com>
2017-01-09 02:32:25 -0500

    initialize method try to clear device state when first valid sample is read, fail after 5 attempts and leave sensor inactive; flush before requesting data

--- mhz14.py
+++ mhz14.py
@@ -1,4 +1,4 @@
-import time
+import utime
 import machine
 
 DEBUG = False
@@ -20,30 +20,64 @@ class MHZ14(object):
     def __init__(self, tx = 12, rx = 14,):
         self._ser = machine.SoftUART(machine.Pin(tx),machine.Pin(rx), baudrate=9600)
         self._data_buff = bytearray(9)
+        self.active = False
         
-    def get_data(self, d = None, timeout = 2.0):
+    def init(self):
+        self._wakeup()
+        
+    def _wakeup(self):
+        #sensor can inititally be in a weird state, so talk to it twice and then flush
         self._ser.write(CMD_REQUEST_PPM)
-        start_time = time.time()
-        while True:
-            if time.time() - start_time > timeout:
-                raise Exception("data request timed out")
-            time.sleep_ms(10)
+        utime.sleep_ms(1000)
+        self._ser.flush()
+        for i in range(5):
+            self._ser.write(CMD_REQUEST_PPM)
+            utime.sleep_ms(10)
             self._ser.readinto(self._data_buff)
-            if DEBUG:
-                print(self._data_buff)
             if self._data_buff[0] == 0xFF: #possibly valid data
-                break
-        db = self._data_buff
-        #compute checksum to validate data
-        checksum = ~sum(db[1:8])+1 & 0xFF
-        if DEBUG:
-            print(db,checksum)
-        if checksum != db[8]:
-            raise Exception("bad checksum")
+                db = self._data_buff
+                #compute checksum to validate data
+                checksum = ~sum(db[1:8])+1 & 0xFF
+                if DEBUG:
+                    print(db,checksum)
+                if checksum == db[8]: #data is valid, so we are ready
+                    self.active = True
+                    break
+            #repeat until we get valid data
+            utime.sleep_ms(1000)
+        else:
+            print("WARNING: Initialization of AM2315 sensor failed!\n\tsetting attribute active=False")
+            self.active = False
+        
+    def get_data(self, d = None, timeout = 2):
         if d is None:
             d = {}
-        d['ppm'] = (256*db[2] + db[3])
-        return d
+        if not self.active:
+            return d
+        else:
+            self._ser.flush()
+            self._ser.write(CMD_REQUEST_PPM)
+            start_time = utime.ticks_ms()
+            while True:
+                if utime.ticks_ms() - start_time > timeout*1000:
+                    raise Exception("data request timed out")
+                utime.sleep_ms(10)
+                self._ser.readinto(self._data_buff)
+                if DEBUG:
+                    print(self._data_buff)
+                if self._data_buff[0] == 0xFF: #possibly valid data
+                    break
+            db = self._data_buff
+            #compute checksum to validate data
+            checksum = ~sum(db[1:8])+1 & 0xFF
+            if DEBUG:
+                print(db,checksum)
+            if checksum != db[8]:
+                print("WARNING: AM2315 sensor sent repsonse with a bad checksum!")
+                d['co2_ppm'] = float('nan')
+            else:
+                d['co2_ppm'] = (256*db[2] + db[3])
+            return d
         
     def calibrate_zero_point(self):
         self._ser.write(CMD_REQUEST_PPM)
