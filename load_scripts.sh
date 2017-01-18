11c9a6ba79a14ed92ab22595cd265d440aa1ee52
Craig Wm. Versek <cversek@gmail.com>
2017-01-18 01:09:41 -0500

    renamed main.py to prevent it from autostarting

--- load_scripts.sh
+++ load_scripts.sh
@@ -5,4 +5,5 @@ ampy -p /dev/ttyUSB0 -b 115200 put data_stream.py
 ampy -p /dev/ttyUSB0 -b 115200 put time_manager.py
 ampy -p /dev/ttyUSB0 -b 115200 put am2315.py
 ampy -p /dev/ttyUSB0 -b 115200 put mhz14.py
-ampy -p /dev/ttyUSB0 -b 115200 put main.py
+ampy -p /dev/ttyUSB0 -b 115200 put boot.py
+ampy -p /dev/ttyUSB0 -b 115200 put datalogger_app.py
